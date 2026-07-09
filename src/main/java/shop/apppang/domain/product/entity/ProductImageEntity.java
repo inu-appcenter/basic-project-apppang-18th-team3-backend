84964d9a54d68dfed25c24511dfc7f2f051cac90
eatyummydance200 <wjdxodud3@gmail.com>
2026-07-09 16:25:54 +0900

    ✨ Feature : 상품 이미지 엔티티 생성

---
+++ src/main/java/shop/apppang/domain/product/entity/ProductImageEntity.java
@@ -0,0 +1,39 @@
+package shop.apppang.domain.product.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "product_images")
+public class ProductImageEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private ProductEntity product;
+
+    @Column(nullable = true, length = 255)
+    private String imageUrl;
+
+    @Column(nullable = true)
+    private Integer displayOrder;
+
+    @Column(nullable = true)
+    private Boolean isMain;
+
+    @Builder
+    public ProductImageEntity(ProductEntity product, String imageUrl, Integer displayOrder, Boolean isMain) {
+        this.product = product;
+        this.imageUrl = imageUrl;
+        this.displayOrder = displayOrder;
+        this.isMain = isMain;
+    }
+}
