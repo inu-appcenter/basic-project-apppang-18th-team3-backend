04a3292c37606e19f762485e46b24d804cb716bc
eatyummydance200 <wjdxodud3@gmail.com>
2026-07-13 01:00:43 +0900

    ✨ Feature : 마이페이지-최근 찾던 상품 API 구현 (#37)

---
+++ src/main/java/shop/apppang/domain/product/repository/ProductImageRepository.java
@@ -0,0 +1,12 @@
+package shop.apppang.domain.product.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import shop.apppang.domain.product.entity.ProductImageEntity;
+
+import java.util.List;
+
+public interface ProductImageRepository extends JpaRepository<ProductImageEntity, Long> {
+
+    List<ProductImageEntity> findByProductIdInAndIsMainTrue(List<Long> productIds);
+
+}
