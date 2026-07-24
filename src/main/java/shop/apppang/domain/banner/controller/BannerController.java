8f4adcab4d25f1aac3e545c8b95a3dedd5026432
pwc1217 <01046883774q@gmail.com>
2026-07-24 19:00:09 +0900

    🪲 Fix : 배너 들어올 때 https:// 로 들어오게 수정, 정렬 쿼리 추가 (#66)
    * 배너 API 구현
    
    * fix: 배너 조회 API S3 절대 경로 적용 및 활성화 배너 정렬 조회 기능 구현
    
    ---------
    
    Co-authored-by: eatyummydance200 <wjdxodud3@gmail.com>

--- src/main/java/shop/apppang/domain/banner/controller/BannerController.java
+++ src/main/java/shop/apppang/domain/banner/controller/BannerController.java
@@ -3,6 +3,7 @@ package shop.apppang.domain.banner.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +12,7 @@ import shop.apppang.domain.banner.service.BannerService;
 
 import java.util.List;
 
-@Tag(name = "메인페이지")
+@Tag(name = "배너 API", description = "메인 홈 배너 관련 API")
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
@@ -19,12 +20,10 @@ public class BannerController {
 
     private final BannerService bannerService;
 
-    @Operation(summary = "메인 배너 목록 조회")
+    @Operation(summary = "메인 홈 배너 목록 조회")
     @GetMapping("/banners")
-    public List<BannerResponse> getBanners() {
-
-        return bannerService.getBanners();
-
+    public ResponseEntity<List<BannerResponse>> getBanners() {
+        return ResponseEntity.ok(bannerService.getBanners());
     }
 
 }
