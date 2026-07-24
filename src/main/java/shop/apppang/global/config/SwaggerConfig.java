32fe40559e5d9cc0c87c0c79b0bf3ff9a6c61710
eatyummydance200 <wjdxodud3@gmail.com>
2026-07-24 20:11:45 +0900

    ✨ Feature : 스웨거 자물쇠(인증 필요 여부) 표시

--- src/main/java/shop/apppang/global/config/SwaggerConfig.java
+++ src/main/java/shop/apppang/global/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package shop.apppang.global.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.media.Content;
 import io.swagger.v3.oas.models.media.MediaType;
 import io.swagger.v3.oas.models.media.Schema;
@@ -10,11 +11,33 @@ import io.swagger.v3.oas.models.responses.ApiResponse;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.customizers.GlobalOpenApiCustomizer;
+import org.springdoc.core.customizers.OperationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.HandlerMethod;
+
+import java.util.List;
+import java.util.Set;
 
 @Configuration
 public class SwaggerConfig {
+
+    // SecurityConfig의 permitAll 목록과 반드시 동기화할 것 (컨트롤러 클래스명#메서드명)
+    private static final Set<String> PUBLIC_OPERATIONS = Set.of(
+            "ProductController#getProducts",
+            "ProductController#getProduct",
+            "ReviewController#getReviews",
+            "CategoryController#getCategories",
+            "BannerController#getBanners",
+            "SearchController#getSuggestions",
+            "ChatController#chat",
+            "AuthController#signup",
+            "AuthController#login",
+            "AuthController#checkEmail",
+            "AuthController#findEmail",
+            "AuthController#verifyPasswordReset",
+            "AuthController#resetPassword"
+    );
 
     @Bean
     public OpenAPI openAPI() {
@@ -27,6 +50,19 @@ public class SwaggerConfig {
                                 .type(SecurityScheme.Type.HTTP)
                                 .scheme("bearer")
                                 .bearerFormat("JWT")));
+    }
+
+    // permitAll API는 자물쇠(인증 필요 표시)를 제거. springdoc이 만든 Operation 모델을 직접 수정하므로
+    // @Operation(security = {})와 달리 확실하게 반영된다 (해당 애너테이션 방식은 springdoc에서 알려진 미동작 이슈가 있음)
+    @Bean
+    public OperationCustomizer publicApiSecurityCustomizer() {
+        return (Operation operation, HandlerMethod handlerMethod) -> {
+            String key = handlerMethod.getBeanType().getSimpleName() + "#" + handlerMethod.getMethod().getName();
+            if (PUBLIC_OPERATIONS.contains(key)) {
+                operation.setSecurity(List.of());
+            }
+            return operation;
+        };
     }
 
     // 모든 API에 공통으로 500 응답 문서를 자동으로 추가
